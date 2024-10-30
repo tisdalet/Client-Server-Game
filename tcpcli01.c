9cc3b1ec07e3da04ac88ec75d55e961bf81b2d14
Tyler Tisdale <tylertisdale1031@gmail.com>
2024-10-29 20:00:24 -0500

    Adding a little, should also have board now

--- tcpcli01.c
+++ tcpcli01.c
@@ -1,10 +1,17 @@
 #include	"unp.h"
+#include	"unp.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h> // for read, write
+#include <arpa/inet.h> // for sockets
 
 int
 main(int argc, char **argv)
 {
 	int					sockfd;
 	struct sockaddr_in	servaddr;
+	char	sendline[MAXLINE], recvline[MAXLINE];
+
 
 	if (argc != 2){
 		printf("usage: tcpcli <IPaddress>");
@@ -15,12 +22,23 @@ main(int argc, char **argv)
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(SERV_PORT);
+	servaddr.sin_port = htons(23456);
 	inet_pton(AF_INET, argv[1], &servaddr.sin_addr);
 
 	connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
 
-	str_cli(stdin, sockfd);		/* do it all */
+	// str_cli(stdin, sockfd); //might not use this anymore
+	while (fgets(sendline, MAXLINE, fp) != NULL) {
+
+		write(sockfd, sendline, strlen(sendline));
+
+		if (read(sockfd, recvline, MAXLINE) == 0){
+			printf("str_cli: server terminated prematurely");
+			exit(1);
+		}
+
+		fputs(recvline, stdout);
+	}
 
 	exit(0);
 }
--- tcpserver01.c
+++ tcpserver01.c
@@ -1,4 +1,14 @@
 #include	"unp.h"
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h> // for read, write
+#include <arpa/inet.h> // for sockets
+
+#define BOARD_SIZE 5
+
+typedef struct {
+    char grid[BOARD_SIZE][BOARD_SIZE];
+} Board;
 
 int
 main(int argc, char **argv)
@@ -7,13 +17,15 @@ main(int argc, char **argv)
 	pid_t				childpid;
 	socklen_t			clilen;
 	struct sockaddr_in	cliaddr, servaddr;
+	Board board;
 
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+	printf("Server is ready to talk...\n");
 
 	bzero(&servaddr, sizeof(servaddr));
 	servaddr.sin_family      = AF_INET;
 	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-	servaddr.sin_port        = htons(SERV_PORT);
+	servaddr.sin_port        = htons(23456);
 
 	bind(listenfd, (SA *) &servaddr, sizeof(servaddr));
 
@@ -21,14 +33,34 @@ main(int argc, char **argv)
 
 	for ( ; ; ) {
 		clilen = sizeof(cliaddr);
+		// Accept connection
 		connfd = accept(listenfd, (SA *) &cliaddr, &clilen);
 
+		// Fork child process
 		if ( (childpid = fork()) == 0) {	/* child process */
-			close(listenfd);	/* close listening socket */
-			str_echo(connfd);	/* process the request */
+			char buf[MAXLINE]; // message from client
+			char buf2[MAXLINE]; // for reponse
+			ssize_t	n;
+
+			close(listenfd); /* close listening socket */
+			
+			while ( (n = read(connfd, buf, MAXLINE)) > 0) {
+				//n = read(connfd, buf, MAXLINE);
+				if (n > 0) {
+					// remove end char
+					buf[n] = '\0';
+					// print
+					fputs(buf, stdout);
+				}
+				// take in front keyboard, stdin
+				fgets(buf2, MAXLINE, stdin);
+				// write to socket to client
+				write(connfd, buf2,strlen(buf2));
+			}
+			close(connfd);
 			exit(0);
 		}
-		close(connfd);			/* parent closes connected socket */
-		//wait(NULL);
+		close(connfd);	/* parent closes connected socket */
+		wait(NULL);
 	}
 }
